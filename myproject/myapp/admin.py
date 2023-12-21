9e20ded39539a13121400862e3031b89274d3b1a
SeongHyeon <ericshyoon24@naver.com>
2023-12-21 20:26:05 +0900

    231221-1

--- myproject/myapp/admin.py
+++ myproject/myapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Blog
+from .models import notice
 # Register your models here.
+admin.site.register(Blog)
+admin.site.register(notice)
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from .models import Blog
+from .models import notice
 # Create your views here.
 
 def layout(request):
@@ -9,7 +10,9 @@ def layout2(request):
     return render(request,'myapp/layout2.html')
 
 def layout3(request):
-    return render(request,'myapp/layout3.html')
+    notices = notice.objects
+    return render(request,'myapp/layout3.html', {'notices':notices})
 
 def layout4(request):
-    return render(request,'myapp/layout4.html')
+    blogs = Blog.objects
+    return render(request,'myapp/layout4.html', {'blogs':blogs})
